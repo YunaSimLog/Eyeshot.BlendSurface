dc8590eac3cb490e944f625181bb6b6b1a2bb76a
yunasim <ynsim@solidit.co.kr>
2024-05-01 16:57:56 +0900

    #😎update: Design 재정의 클래스 작성

---
+++ MyDesign.cs
@@ -0,0 +1,44 @@
+﻿using devDept.Eyeshot;
+using devDept.Eyeshot.Control;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Eyeshot.BlendSurface
+{
+    public class MyDesign : Design
+    {
+        private string _text;
+        private System.Drawing.Point _mouseLocation;
+
+        protected override void DrawOverlay(DrawSceneParams data)
+        {
+            // 텍스트 그리기
+            DrawText(_mouseLocation.X, Size.Height - _mouseLocation.Y + 10, _text, new System.Drawing.Font("Tahoma", 8.25f), Color.Black, ContentAlignment.BottomLeft);
+
+            base.DrawOverlay(data);
+        }
+
+        public void SetTipText(string text)
+        {
+            _text = text;
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            _mouseLocation = e.Location;
+
+            // 뷰포트 곡면을 칠한다.
+            PaintBackBuffer();
+
+            // Drawings을 통합한다.
+            SwapBuffers();
+
+            base.OnMouseMove(e);
+        }
+    }
+}
